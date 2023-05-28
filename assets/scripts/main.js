f9524af07a43a452eb25da1cf708dc5833b2ea38
Doheon Kim <peterjhms1922@gmail.com>
2023-05-27 19:33:39 -0700

    add

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -74,9 +74,11 @@ function saveRecipesToStorage(recipes) {
 function initFormHandler() {
 
   // B2. TODO - Get a reference to the <form> element
+  let reference = document.querySelector('form');
   
   // B3. TODO - Add an event listener for the 'submit' event, which fires when the
   //            submit button is clicked
+  reference.addEventListener('submit')
 
   // Steps B4-B9 will occur inside the event listener from step B3
   // B4. TODO - Create a new FormData object from the <form> element reference above
